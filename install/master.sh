b0bce9cf8d1bbbd510e8d6e562e808d407f598ee
Denis Volnenko <orion-company@yandex.ru>
2021-07-02 04:28:01 +0000

    Update install/master.sh

--- install/master.sh
+++ install/master.sh
@@ -2,5 +2,15 @@
 
 sudo apt-get update
 
-sudo apt-get install -y mc curl fail2ban apt-transport-https
+sudo apt-get install -y mc curl fail2ban apt-transport-https ntpdate
 sudo apt-get install -y docker.io
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
+apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+apt-get install -y kubeadm kubelet kubectl kubernetes-cni
+
+kubeadm init --pod-network-cidr=10.244.0.0/16
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
